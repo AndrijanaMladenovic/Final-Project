35cef60b2e55733382a837eb8fd0598eb66356bc
Stefan <stefan.bojkovic6@gmail.com>
2023-03-28 20:18:41 +0200

    Feature-4

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
 import { Header } from './components/Header';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Footer } from './components/Footer';
+import Main from './components/Main';
+import { Routes, Route, BrowserRouter } from 'react-router-dom'
 
 function App() {
   return (
     <>
-    <Header/>
-    <Footer/>
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route element={<Main />} path="/">
+          </Route>
+        </Routes>
+        <Footer />
+      </BrowserRouter>
     </>
   );
 }
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,7 @@
 export const Footer = () => {
-   return <footer className="container-fluid bg-primary d-flex justify-content-around">
+   return <footer className="container-fluid bg-primary d-flex justify-content-around fixed-bottom p-4">
         <div className=" mr-auto ">
-            <p className="text-light">@BitProject 2023</p>
+            <p className="text-light mb-0">© BitProject 2023</p>
         </div>
         
 
