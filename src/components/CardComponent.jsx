51dc1cb9023a0293526049bdd274bc94f46106f8
Andrijana Mladenovic <andrijanaanci@gmail.comm>
2023-03-30 17:11:24 +0200

    project

--- src/components/CardComponent.jsx
+++ src/components/CardComponent.jsx
@@ -1,28 +1,25 @@
-import Card from 'react-bootstrap/Card';
-
+import Card from "react-bootstrap/Card";
 
 const CardComponent = () => {
+  // let navigate = useNavigate();
 
-        // let navigate = useNavigate();
-
-    return (
-
-        <main className='container-fluid justify-content-center text-center d-flex flex-wrap pt-2'>
-
-            <Card className="col-lg-2 col-md-4 col-sm-7 m-3 border-primary shadow" style={{ minWidth: "20%" }}>
-                <Card.Img className="pe-4 ps-4 pt-4" variant="top"
-                // onClick={()=>{navigate(`/${item.id}`)}}
-                />
-                <Card.Body>
-                    <Card.Title className=''>name</Card.Title>
-                    <Card.Text >
-                        email
-                    </Card.Text>
-                </Card.Body>
-            </Card>
-
-        </main>
-    )
-}
+  return (
+    <main className="container-fluid justify-content-center text-center d-flex flex-wrap pt-2">
+      <Card
+        className="col-lg-2 col-md-4 col-sm-7 m-3 border-primary shadow"
+        style={{ minWidth: "20%" }}>
+        <Card.Img
+          className="pe-4 ps-4 pt-4"
+          variant="top"
+          // onClick={()=>{navigate(`/${item.id}`)}}
+        />
+        <Card.Body>
+          <Card.Title className="">name</Card.Title>
+          <Card.Text>email</Card.Text>
+        </Card.Body>
+      </Card>
+    </main>
+  );
+};
 
 export default CardComponent;
